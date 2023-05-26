f686f9dfca4365fc1a0ac40e4e6cf5eb3bbf39a4
mdolhare <mdolhare@itba.edu.ar>
2023-05-26 18:44:14 -0300

    mas prolijo

--- helper.py
+++ helper.py
@@ -3,15 +3,42 @@ import numpy as np
 from sklearn.metrics import f1_score
 from tensorflow import keras
 
-# Define a custom callback to compute the F1 macro score
-class MetricsCallback(keras.callbacks.Callback):
-    def __init__(self):
-        super(MetricsCallback, self).__init__()
-    def  on_train_begin(self,logs={}):
-        self.f1_macro=[]
-    def on_epoch_end(self, epoch, logs=None):
-        y_pred=self.model.predict(testX).round()
-        y_true=y_test
-        score=f1_score(y_true, y_pred, average='macro')
-        self.f1_macro.append(score)
-        print(" F1 macro :",score)
+def zero50(arr):
+    # Determine the threshold value for the top 50% elements
+    threshold = np.percentile(arr, 50)
+
+    # Create a copy of the original array
+    arr_copy = np.copy(arr)
+
+    # Set elements greater than the threshold to zero
+    arr_copy[arr_copy > threshold] = 0
+
+
+    return arr_copy
+
+'''
+arr = np.array([100, 389, 324, 235356, 36, 2, 5, 2, 7, 9, 3, 6, 8, 4])
+result = set_top_50_percent_to_zero(arr)
+print(result)
+'''
+
+
+
+
+def elim50(weightsR1, weightsR2):
+    shape  = weightsR1.shape
+    flat1 = weightsR1.flatten()
+    flat2 = weightsR2.flatten()
+    marks = np.zeros(len(flat1))
+        
+    marks = flat1 - flat2
+
+    marks = zero50(marks)
+
+    flat1[marks==0] = 0
+
+    unflat1 = flat1.reshape(shape)
+
+    return unflat1
+
+
